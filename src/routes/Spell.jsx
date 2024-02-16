c9b35d90ab9c4cf0c94cba6e739048b0fedfb87c
KER <ker26772@gmail.com>
2024-02-16 16:44:57 +0900

    1234

--- src/routes/Spell.jsx
+++ src/routes/Spell.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function Spell() {
+  const urlToFetch = "https://harry-potter-api-en.onrender.com/spells";
+  const [spells, setSpells] = useState([]);
+
+  useEffect(() => {
+    fetch(urlToFetch)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const spellsArray = data.spells || []; // 수정된 부분
+        setSpells(spellsArray);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
   return (
-    <div>Spell</div>
-  )
+    <div>
+      {spells.map((sp) => (
+        <div key={sp.id}>
+          <div className="text-white">{sp.spell}</div>
+          <div>{sp.use}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
