27489365fba3a69002f8229b197cb0364f82fc03
KER <ker26772@gmail.com>
2024-02-16 14:56:30 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import Character from "./routes/Character";
 import MainPage from "./routes/MainPage";
-import { BrowserRouter as Router, createBrowserRouter } from "react-router-dom";
-import Spell from "./routes/Spell";
-import Book from "./routes/Book";
 
 
 function App() {
